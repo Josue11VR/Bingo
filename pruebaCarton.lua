79840825b5c631ef81a2203010e40cefe4d0fd51
geramarinfer03 <geramarinfer03@gmail.com>
2017-09-30 10:50:58 -0600

    cosas
    no importante

--- pruebaCarton.lua
+++ pruebaCarton.lua
@@ -1,4 +1,7 @@
 require "cartonBingo"
-carton = {{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}}
-llenarCarton(carton)
-imprimirCarton(carton)
+--carton = {{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}}
+carton2 = {{02,11,16,09,14},{25,28,30,18,20},{32,44,0,38,36},{50,46,58,59,49},{70,75,62,68,64}}
+numerosMarcados = {}
+--llenarCarton(carton)
+imprimirCarton(carton2)
+
